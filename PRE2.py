6a759ea0448a3f2fe1b0ad626ed9e15f73131e0b
evelynmarflo <pericotita10@gmail.com>
2015-12-11 17:17:15 -0500

    cod raiz

---
+++ PRE2.py
@@ -0,0 +1,6 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from math import sqrt
+>>> ###pregunta2
+>>> def euclidean(v1,v2):
+return sqrt(sum([v1[i]-v2[i])**2 for i range (len (v1))]))
