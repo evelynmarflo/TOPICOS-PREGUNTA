9e8d4c8250ba302fb9a29dd72d1ea09ffc54ed14
evelynmarflo <pericotita10@gmail.com>
2015-12-11 17:17:05 -0500

    codigo raiz

---
+++ PRE3.py
@@ -0,0 +1,6 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from math import sqrt
+>>>  ###pregunta3
+>>> def manjathan(v1,v2):
+return abs(sum([v1[i]-v2[i]) for i in range (len(v1))]))
