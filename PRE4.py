99d005dad695dc77af4d201cd2416f82bb6cdce4
evelynmarflo <pericotita10@gmail.com>
2015-12-11 17:16:57 -0500

    no me sale pero seguire intentando

---
+++ PRE4.py
@@ -0,0 +1,28 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from math import sqrt
+>>> def  distancia  (v1, v2):
+	c1, c2, SHR =0, 0, 0
+	para estar i en  Rango  (len (v1)):
+		
+SyntaxError: invalid syntax
+>>> def  distancia  (v1, v2):
+  c1, c2, SHR =0, 0, 0
+  
+  párrafo i en  Rango  (len (v1)):
+    SI v1 [i] =! 0: c1 + = 1
+    SI v2 [i] =! 0: c2 + = 1
+    SI v1 [i] =! 0  y v2 [i] =! 0: SHR + = 1
+ volver  float (SHR) / (c1 + c2 - SHR)
+ 
+SyntaxError: invalid syntax
+>>> 
+>>> #pregunta 4
+>>> def  distancia  (v1, v2):
+  c1, c2, SHR =0, 0, 0
+  
+  párrafo i en  Rango  (len (v1)):
+    SI v1 [i] =! 0: c1 + = 1
+    SI v2 [i] =! 0: c2 + = 1
+    SI v1 [i] =! 0  y v2 [i] =! 0: SHR + = 1
+ volver  float (SHR) / (c1 + c2 - SHR)
